c4c18de9688880dbf41bfb20255c835b96e07fb0
Tsvety <19126@uktc-bg.com>
2025-02-18 14:08:00 +0200

    first commit

---
+++ Section_3/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+   /*char my_name[] = "";
+
+   printf("Enter your name:\n");
+
+   scanf("%s", my_name);*/   
+
+   printf("Hi, my name is Tsvety");
+
+   return 0;
+
+}
